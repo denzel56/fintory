5e50f401fcc72b927a46ced90270d4b84c3beb61
Denzel <golodkov.den@gmail.com>
2026-06-30 13:14:24 +0300

    feat: add electron process skeleton

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'dist-electron']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -17,6 +17,12 @@ export default defineConfig([
     ],
     languageOptions: {
       globals: globals.browser,
+    },
+  },
+  {
+    files: ['src/main/**/*.ts'],
+    languageOptions: {
+      globals: globals.node,
     },
   },
 ])
